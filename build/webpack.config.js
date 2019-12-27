3485d224deab465a2e6e94a3e551a9ac1600d0b8
丁浩然 <netcasemail@163.com>
2019-12-27 11:02:56 +0800

    区分生产环境和开发环境

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,61 +1,63 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // 将编译后的文件名插入到html文件中
+const path = require('path')
 const {
     CleanWebpackPlugin
-} = require('clean-webpack-plugin'); // 编译之前删除之前编译的文件
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 将css合并并以外链方式引入html文件
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin') // 拆分成多个css并引入
-let indexLess = new ExtractTextWebpackPlugin('index.less');
-let indexCss = new ExtractTextWebpackPlugin('index.css');
-const Webpack = require('webpack')
-
+} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const devMode = process.argv.indexOf('--mode=production') === -1;
 module.exports = {
-    mode: 'development', // 开发模式
     entry: {
-        main: path.resolve(__dirname, '../src/main.js'),
-        header: path.resolve(__dirname, '../src/header.js')
+        main: path.resolve(__dirname, '../src/main.js')
     },
     output: {
-        filename: '[name].[hash:8].js', // 打包后的文件名称
-        path: path.resolve(__dirname, '../dist') // 打包后的目录
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'js/[name].[hash:8].js',
+        chunkFilename: 'js/[name].[hash:8].js'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../public/index.html'),
-            filename: 'index.html',
-            chunks: ['main'] // 与入口文件对应的模块名
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../public/header.html'),
-            filename: 'header.html',
-            chunks: ['header'] // 与入口文件对应的模块名
-        }),
-        new CleanWebpackPlugin(),
-        require('autoprefixer'), // postcss 添加css前缀
-        new MiniCssExtractPlugin({
-            filename: "[name].[hash].css",
-            chunkFilename: "[id].css",
-        }),
-        indexLess,
-        indexCss,
-        new Webpack.HotModuleReplacementPlugin() // 热更新
-    ],
     module: {
         rules: [{
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
                 test: /\.css$/,
-                use: indexCss.extract({
-                    use: ['css-loader']
-                })
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: "../dist/css/",
+                        hmr: devMode
+                    }
+                }, 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [require('autoprefixer')]
+                    }
+                }]
             },
             {
                 test: /\.less$/,
-                use: indexLess.extract({
-                    use: ['css-loader', 'less-loader']
-                })
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: "../dist/css/",
+                        hmr: devMode
+                    }
+                }, 'css-loader', 'less-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [require('autoprefixer')]
+                    }
+                }]
             },
             {
-                test: /\.(jpe?g|png|gif)$/i, //图片文件
-                use: [{
+                test: /\.(jep?g|png|gif)$/,
+                use: {
                     loader: 'url-loader',
                     options: {
                         limit: 10240,
@@ -66,11 +68,11 @@ module.exports = {
                             }
                         }
                     }
-                }]
+                }
             },
             {
-                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //媒体文件
-                use: [{
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                use: {
                     loader: 'url-loader',
                     options: {
                         limit: 10240,
@@ -81,38 +83,39 @@ module.exports = {
                             }
                         }
                     }
-                }]
+                }
             },
             {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, // 字体
-                use: [{
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+                use: {
                     loader: 'url-loader',
                     options: {
                         limit: 10240,
                         fallback: {
                             loader: 'file-loader',
                             options: {
-                                name: 'fonts/[name].[hash:8].[ext]'
+                                name: 'media/[name].[hash:8].[ext]'
                             }
                         }
                     }
-                }]
-            },
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                },
-                exclude: /node_modules/
-            },
+                }
+            }
         ]
     },
-    devServer: {
-        port: 3000,
-        hot: true,
-        contentBase: '../dist'
+    resolve: {
+        alias: {
+            ' @': path.resolve(__dirname, '../src')
+        },
+        extensions: ['*', '.js', '.json']
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+        })
+    ]
 }
